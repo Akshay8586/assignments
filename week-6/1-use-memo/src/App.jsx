682c3fb7faba7ec49bfb83263091e4ca29d15c27
Akshay8586 <akshaydhandhi8@gmail.com>
2024-01-09 22:08:05 +0530

    Solved week-6 1-use-memo

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import './App.css'
-import { Assignment1 } from './components/Assignment2'
-import { Assignment2 } from './components/Assignment1'
+import { Assignment1 } from './components/Assignment1'
+import { Assignment2 } from './components/Assignment2'
 import Assignment3 from './components/Assignment3'
 
 function App() {
+  const [count,setCount]=useState(0);
+
+  function increaseCount(){
+    setCount(count+1);
+  }
+
+
   return (
     <>
       <Assignment1 />
-      {/* <Assignment2 /> */}
-      {/* <Assignment3 /> */}
+      <button onClick={increaseCount}>Count {count}</button>
+      {<Assignment2 /> }
+      <Assignment3 />
     </>
   )
 }
